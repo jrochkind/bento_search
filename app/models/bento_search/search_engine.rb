b7ee4b545c5325ec48c9d508f9dc913838848158
Jonathan Rochkind <jonathan@dnil.net>
2012-05-14 14:09:56 -0400

    required key behavior

--- app/models/bento_search/search_engine.rb
+++ app/models/bento_search/search_engine.rb
@@ -2,9 +2,25 @@ require 'active_support/concern'
 require 'active_support/core_ext/module/delegation'
 require 'confstruct'
 
+
 module BentoSearch
   # Module mix-in for bento_search search engines. 
+  #
+  # ==Using a SearchEngine 
+  #
+  # == Implementing a SearchEngine
+  #
   # `include BentoSearch::SearchEngine`
+  #
+  #  a SearchEngine's state should not be search-specific, but
+  #  is configuration specific. Don't store anything specific
+  #  to a specific search in iVars. 
+  #
+  #  Do implement `#search(*args)`
+  # 
+  #  Do use HTTPClient, if possible, for http searches, 
+  #  using a class-level HTTPClient to maintain persistent connections. 
+  #
   module SearchEngine
     extend ActiveSupport::Concern
     
@@ -17,6 +33,16 @@ module BentoSearch
     # Confstruct::Configuration. 
     def initialize(aConfiguration)
       self.configuration = aConfiguration
+      # check for required keys
+      
+      if self.class.respond_to?(:required_configuration)
+        self.class.required_configuration.each do |required_key|
+          if self.configuration.lookup!(required_key, "**NOT_FOUND**") == "**NOT_FOUND**"
+            raise ArgumentError.new("#{self.class.name} requires configuration key #{required_key}")
+          end
+        end
+      end
+      
     end
     
   end
--- test/unit/search_engine_test.rb
+++ test/unit/search_engine_test.rb
@@ -4,21 +4,32 @@ require 'test_helper'
 
 class SearchEngineTest < ActiveSupport::TestCase
   
-  def setup
-    @dummy_class = Class.new do
-      include BentoSearch::SearchEngine
+    def setup
+      @dummy_class = Class.new do
+        include BentoSearch::SearchEngine
+        
+        def self.required_configuration
+          ["required.key"]
+        end
+      end
     end
-  end
   
 
     test "takes configuration" do
-      conf = Confstruct::Configuration.new( :foo => "foo", :bar => "bar" )
+      conf = Confstruct::Configuration.new( :foo => "foo", :bar => "bar", :required => {:key => "required key"} )
       engine = @dummy_class.new(conf)
       
       assert_not_nil engine.configuration
       assert_equal engine.configuration.foo, "foo"
       assert_equal engine.configuration.bar, "bar"      
     end
-
+    
+    test "required configuration keys" do
+      conf = Confstruct::Configuration.new( :foo => "foo", :bar => "bar" )
+      assert_raise ArgumentError do
+        @dummy_class.new(conf)
+      end
+      
+    end
 
 end
