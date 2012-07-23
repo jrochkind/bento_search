39b339a671082299fc342c0b545a7860d575905a
Jonathan Rochkind <jonathan@dnil.net>
2012-07-23 12:57:35 -0400

    remove errant debugger line

--- app/helpers/bento_search_helper.rb
+++ app/helpers/bento_search_helper.rb
@@ -87,7 +87,6 @@ module BentoSearchHelper
   # Prepare a title in an H4, with formats in parens in a <small> (for
   # bootstrap), linked, etc. 
   def bento_item_title(item)
-    require 'debugger'
     content_tag("h4", :class => "bento_item_title") do
       safe_join([
         link_to_unless( item.link.blank?, item.complete_title, item.link ),
