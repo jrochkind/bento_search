f766bdef4017a3e9367b2326098fb27407f442b4
Jonathan Rochkind <jonathan@dnil.net>
2012-07-26 16:12:39 -0400

    Pagination object for UI help

---
+++ app/models/bento_search/results/pagination.rb
@@ -0,0 +1,58 @@
+
+# An object intended to be compatible with kaminari for pagination,
+# although kaminari doesn't doc/spec exactly what you need, so might
+# break in future. Could be useful for your own custom pagination too. 
+#
+# You don't normally create one of these yourself, you get one returned
+# from Results#pagination
+class BentoSearch::Results::Pagination
+  
+  def initialize(total, normalized_args)
+    @total_count = total
+    @per_page = normalized_args[:per_page] || 10
+    @current_page = normalized_args[:page]  || 1
+    @start_record = (normalized_args[:start] || 0) + 1 
+  end
+    
+  def current_page
+    @current_page
+  end
+  
+  # 1-based start, suitable for showing to user
+  # Can be 0 for empty result set. 
+  def start_record
+    [@start_record, count_records].min
+  end
+  
+  # 1-based last record in window, suitable for showing to user.
+  # Can be 0 for empty result set.   
+  def end_record
+    [start_record + per_page - 1, count_records].min
+  end
+  
+  def count_records
+    @total_count
+  end
+  
+  def total_pages
+    (@total_count.to_f / @per_page).ceil
+  end
+  # kaminari wants both, weird. 
+  alias num_pages total_pages
+
+  
+  def first_page?
+    current_page == 1
+  end
+  
+  def last_page?
+    current_page >= total_pages
+  end
+     
+  def per_page
+    @per_page
+  end
+  # kaminari wants it called this.
+  alias limit_value per_page
+  
+end
---
+++ test/unit/pagination_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+# Test the BentoSearch::Results::Pagination object
+class PaginationTest < ActiveSupport::TestCase
+  Pagination = BentoSearch::Results::Pagination
+  
+  def test_implicit_page
+    pag = Pagination.new(100, {:per_page => 10})
+    
+    assert_equal 1, pag.current_page
+    assert_equal 1, pag.start_record
+    assert_equal 10, pag.end_record
+    assert pag.first_page?
+    assert ! pag.last_page?
+    assert_equal 100, pag.count_records
+    assert_equal 10, pag.total_pages
+    assert_equal 10, pag.per_page    
+  end
+  
+  def test_last_page
+    pag = Pagination.new(100, {:page => 5, :start => 80, :per_page => 20})
+    
+    assert_equal 5, pag.current_page
+    assert_equal 81, pag.start_record
+    assert_equal 100, pag.end_record
+    assert ! pag.first_page?
+    assert pag.last_page?
+    assert_equal 100, pag.count_records
+    assert_equal 5, pag.total_pages
+    assert_equal 20, pag.per_page
+  end
+  
+  def test_uneven_pages
+    pag = Pagination.new(95, {:page => 10, :start => 90, :per_page => 10})
+    
+    assert_equal 10, pag.current_page
+    assert_equal 91, pag.start_record
+    assert_equal 95, pag.end_record
+    assert ! pag.first_page?
+    assert pag.last_page?
+    assert_equal 95, pag.count_records
+    assert_equal 10, pag.total_pages
+    assert_equal 10, pag.per_page
+  end
+    
+    
+  
+  
+end
