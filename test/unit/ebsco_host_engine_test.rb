ffa122e2789dbc24d7a75a8bcbe8d3ddf90be4bd
Jonathan Rochkind <jonathan@dnil.net>
2012-10-24 18:03:02 -0400

    ebscohost, make test method actually named right to run

--- test/unit/ebsco_host_engine_test.rb
+++ test/unit/ebsco_host_engine_test.rb
@@ -58,13 +58,14 @@ class EbscoHostEngineTest < ActiveSupport::TestCase
     assert_equal ["date"], query_params["sort"]
   end
   
-  def fielded_construction
+  def test_fielded_construction
     url = @engine.query_url(:query => "cancer", :search_field => "SU")
     
     query_params = CGI.parse( URI.parse(url).query )
     
     assert_equal ["(SU cancer)"], query_params["query"]
   end
+  
   
   def test_prepare_query
     query = @engine.ebsco_query_prepare('one :. ; two "three four" and NOT OR five')
