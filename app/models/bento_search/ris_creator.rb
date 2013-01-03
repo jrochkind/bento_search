4557e8f2b2f6dc0bf8bdb0644675b63b65c7bee6
Jonathan Rochkind <jonathan@dnil.net>
2013-01-03 13:17:51 -0500

    RISCreator for RIS exports

---
+++ app/models/bento_search/ris_creator.rb
@@ -0,0 +1,155 @@
+module BentoSearch
+  #
+  # Export a BentoSearch::ResultItem in RIS format, as a file
+  # to import into EndNote etc, or in a callback for Refworks export, etc. 
+  #
+  # RISCreator.new( result_item ).export
+  #
+  # Best spec/docs for RIS format seems to be at 
+  # http://www.refman.com/support/risformat_intro.asp
+  # Download zip file there, pay attention to excel spreadsheet
+  # as well as PDF overview. 
+  #
+  # But note this 'spec' is often ignored/violated, even by the vendors
+  # who wrote it. Wikipedia at http://en.wikipedia.org/wiki/RIS_(file_format)#Tags
+  # contains some additional tags not mentioned in 'spec'. 
+  class RISCreator
+    def initialize(i)
+      @item = i
+      @ris_format = translate_ris_format
+    end
+    
+    def export
+      out = "".force_encoding("UTF-8")
+      
+      out << tag_format("TY", @ris_format)
+      
+      out << tag_format("TI", @item.title)
+      
+      @item.authors.each do |author|
+        out << tag_format("AU", format_author_name(author))
+      end
+      
+      out << tag_format("PY", @item.year)
+      out << tag_format("DA", format_date(@item.publication_date))
+      
+      out << tag_format("LA", @item.language_str)
+      
+      out << tag_format("VL", @item.volume)
+      out << tag_format("IS", @item.issue)
+      out << tag_format("SP", @item.start_page)
+      out << tag_format("EP", @item.end_page)
+      
+      out << tag_format("T2", @item.source_title)
+      
+      # ISSN and ISBN both share SN, sigh. 
+      out << tag_format("SN", @item.issn)
+      out << tag_format("SN", @item.isbn)
+      out << tag_format("DO", @item.doi)
+      
+      out << tag_format("PB", @item.publisher)
+      
+      out << tag_format("AB", @item.abstract)
+      
+      # include main link and any other links?
+      out << tag_format("UR", @item.link)
+      @item.other_links.each do |link|
+        out << tag_format("UR", link.url)
+      end
+      
+      # end with blank lines, so multiple ones can be concatenated for
+      # a file. 
+      out << "\r\nER  - \r\n\r\n"
+    end
+    
+    @@format_map = {
+      # bento_search doesn't distinguish between journal, magazine, and newspaper,
+      # RIS does, sorry, we map all to journal article. 
+      "Article"         => "JOUR",
+      "Book"            => "BOOK",
+      "Movie"           => "MPCT",
+      "MusicRecording"  => "MUSIC",
+      #"Photograph"     => "GEN",
+      "SoftwareApplication" => "COMP",
+      "WebPage"         => "ELEC",
+      "VideoObject"     => "VIDEO",
+      "AudioObject"     => "SOUND",
+      :serial           => "SER",
+      :dissertation     => "THES",
+      :conference_paper => "CPAPER",
+      :conference_proceedings => "CONF",
+      :report           => "RPRT",
+      :book_item        => "CHAP"      
+    }
+    
+    # based on current @item.format, output
+    # appropriate RIS format string
+    def translate_ris_format
+      # default "GEN"=generic if unknown 
+      @@format_map[@item.format] || "GEN"
+    end
+    
+    # Formats refworks tag/value line and returns it. 
+    # 
+    # Returns empty string if you pass in an empty value though. 
+    # 
+    # "Each six-character tag must be in the following format:
+    # "<upper-case letter><upper-case letter or number><space><space><dash><space>"
+    #
+    # "Each tag and its contents must be on a separate line, 
+    # preceded by a "carriage return/line feed" (ANSI 13 10)."
+    #
+    # "Note, however, that the asterisk (character 42)
+    # is not allowed in the author, keywords or periodical name fields."
+    #
+    # The spec also seems to say ascii-only, but I don't think that's true
+    # for actually existing software, we do utf-8. 
+    #
+    # Refworks MAY require unicode composed normalization if it accepts utf8
+    # at all. but not doing that yet. http://bibwild.wordpress.com/2010/04/28/refworks-problems-importing-diacritics/
+    def tag_format(tag, value)
+      return "" if value.blank?
+      
+      raise ArgumentError.new("Illegal RIS tag") unless tag =~ /[A-Z][A-Z0-9]/
+      
+      # "T2" seems to be the only "journal name field", which is
+      # mentioned along with these others as not being allowed to contain
+      # asterisk. 
+      if ["AU", "A2", "A3", "A4", "KW", "T2"].include? tag
+        value = value.gsub("*", " ")
+      end
+      
+      return "\r\n#{tag}  - #{value}"
+    end    
+    
+    # Take a ruby Date and translate to RIS date format
+    # "YYYY/MM/DD/other info"
+    # 
+    # returns nil if input is nil. 
+    def format_date(d)
+      return nil if d.nil?
+      
+      return d.strftime("%Y/%m/%d")
+    end
+    
+    # RIS wants `Last, First M.`, we'll do what we can. 
+    def format_author_name(author)
+      if author.last.present? && author.first.present?
+        str = "#{author.last}, #{author.first}"
+        if author.middle.present?
+          middle = author.middle
+          middle += "." if middle.length == 1
+          str += " #{middle}"
+        end
+        return str
+      elsif author.display.present?
+        return author.display
+      elsif author.last.present?
+        return author.last?
+      else
+        return nil
+      end        
+    end
+    
+  end
+end
---
+++ test/unit/ris_creator_test.rb
@@ -0,0 +1,159 @@
+require 'test_helper'
+
+class RISCreatorTest < ActiveSupport::TestCase
+  RISCreator = BentoSearch::RISCreator
+  ResultItem = BentoSearch::ResultItem
+  Author     = BentoSearch::Author
+  
+  # Parses an RIS file into a simpel data structure easier to test:
+  # an array of two element arrays, where each two element array is 
+  # (tag, field).  This method MAY also assert that the thing basically
+  # looks like a proper RIS file. 
+  def ris_parse(str)
+    assert_match /\A\r\n/, str, "RIS records begin with \r\n seperator"
+    str.gsub!(/\A(\r\n)+/, '') # remove any at the beginning, multiple are fine
+    
+    assert_match /\r\n\Z/, str, "Our RIS records end up with \r\n so they are concat-able with blank lines in between"
+    str.gsub!(/(\r\n)+\Z/, '') # remove any at the end, multiple are fine
+    
+    lines = str.split("\r\n", -1)
+            
+    assert_present lines, "RIS files are composed of multiple lines seperated by \r\n"
+            
+    # trim blank lines off beginning or end, they're allowed. Also "ER" tag
+    # off the end, make sure it's there then trim it. 
+    assert_equal "ER  - ", lines.last, "RIS records end in `ER  - `"
+    lines.pop # throw it away now. 
+    
+    return lines.collect do |line|            
+      line =~ /^([A-Z][A-Z0-9])  - (.*)$/
+      tag = $1 ; value = $2
+      assert (tag.present? && value.present?), "Individual RIS lines are composed of `TG  - VALUE`, not `#{line}`"
+      [tag, value]
+    end
+  end
+  
+  
+  @@article = ResultItem.new(
+    :format => "Article",
+    :title => "Some Article",
+    :source_title => "Some Journal",
+    :issn => "12345678",
+    :volume => "10",
+    :issue => "3",
+    :start_page => "100",
+    :end_page => "110",
+    :doi => '10.1000/182',
+    :abstract => "This is an abstract",
+    :authors => [
+      Author.new(:first => "John", :last => "Smith", :middle => "Q"),
+      Author.new(:first => "Maria", :last => "Lopez"),
+      Author.new(:display => "Some Guy")
+    ]
+  )
+  
+  @@article_with_full_date = ResultItem.new(
+    :format => "Article",
+    :title => "Something",
+    :source_title => "Some Magazine",
+    :start_page => "123",
+    :publication_date => Date.new(2011,9,1)
+  )
+  
+  @@book = ResultItem.new(
+    :format => "Book",
+    :title => "Some Book",
+    :isbn => "1234567890",
+    :publisher => "Some Publisher",
+    :year => "1990",
+    :authors => [
+      Author.new(:first => "John", :last => "Smith", :middle => "Q")
+    ]
+  )
+  
+  @@book_chapter = ResultItem.new(
+    :format   => :book_item,
+    :title    => 'Some Chapter',
+    :source_title => "Book Title",
+    :year     => '1991',
+    :publisher => "Some Pub",
+    :start_page => '10',
+    :authors => [
+      Author.new(:first => "John", :last => "Smith", :middle => "Q")
+    ]
+  )
+  
+  @@dissertation = ResultItem.new(
+    :format => :dissertation,
+    :title => "Areoformation and the new science",
+    :publisher => "University of Mars",
+    :year => "2150",
+    :authors => [
+      Author.new(:display => "mztlbplk q. frakdf")
+    ]
+  )
+
+
+  def test_article        
+    lines = ris_parse RISCreator.new(@@article).export    
+    
+    assert_include lines, ['TY', 'JOUR']
+    assert_include lines, ['TI', @@article.title]
+    assert_include lines, ['T2', @@article.source_title]
+    assert_include lines, ['SN', @@article.issn]
+    assert_include lines, ['VL', @@article.volume]
+    assert_include lines, ['IS', @@article.issue]
+    assert_include lines, ['SP', @@article.start_page]    
+    assert_include lines, ['EP', @@article.end_page]
+    assert_include lines, ['DO', @@article.doi]
+    assert_include lines, ['AB', @@article.abstract]
+
+    assert_include lines, ['AU', "Smith, John Q."]
+    assert_include lines, ['AU', "Lopez, Maria"]
+    assert_include lines, ['AU', "Some Guy"]
+  end
+  
+  def test_book
+    lines = ris_parse RISCreator.new(@@book).export
+    
+    assert_include lines, ['TY', 'BOOK']
+    assert_include lines, ['TI', @@book.title]
+    assert_include lines, ['PY', @@book.year]
+    assert_include lines, ['PB', @@book.publisher]
+    assert_include lines, ['SN', @@book.isbn]
+    
+    assert_include lines, ['AU', "Smith, John Q."]
+  end
+  
+  def test_book_chapter
+    lines = ris_parse RISCreator.new(@@book_chapter).export
+    
+    assert_include lines, ['TY', 'CHAP']
+    
+    assert_include lines, ['TI', @@book_chapter.title]
+    assert_include lines, ['T2', @@book_chapter.source_title]
+    assert_include lines, ['PY', @@book_chapter.year]
+    assert_include lines, ['PB', @@book_chapter.publisher]
+    assert_include lines, ['SP', @@book_chapter.start_page]
+  end
+  
+  def test_dissertation
+    lines = ris_parse RISCreator.new(@@dissertation).export
+    
+    assert_include lines, ['TY', 'THES']
+    
+    assert_include lines, ['TI', @@dissertation.title]
+    assert_include lines, ['PB', @@dissertation.publisher]
+    assert_include lines, ['PY', @@dissertation.year]
+  end
+  
+  def test_article_with_full_date
+    lines = ris_parse RISCreator.new(@@article_with_full_date).export
+    
+    assert_include lines, ['TY', 'JOUR']
+    
+    assert_include lines, ['DA', '2011/09/01']    
+  end
+  
+    
+end
