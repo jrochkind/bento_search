543ef221b73d4d6ffd7bee7cb9939009b18765c2
Jonathan Rochkind <jonathan@dnil.net>
2013-06-18 13:44:28 -0400

    summon, tests, highlighting, test for close bold too

--- test/search_engines/summon_engine_test.rb
+++ test/search_engines/summon_engine_test.rb
@@ -166,9 +166,10 @@ class SummonEngineTest < ActiveSupport::TestCase
     results = @engine.search("cancer")
     
     first = results.first
-    
+
     assert_include first.title, '<b class="bento_search_highlight">' 
-    
+    assert_include first.title, '</b>' 
+
     assert first.title.html_safe?, "title is HTML safe"        
   end
   
