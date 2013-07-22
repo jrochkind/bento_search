691cef49b9004eaf5c2505aa61fa9f69b81a82b9
Jonathan Rochkind <jonathan@dnil.net>
2013-07-22 16:23:53 -0400

    whitespace

--- test/unit/result_item_test.rb
+++ test/unit/result_item_test.rb
@@ -4,51 +4,51 @@ require 'test_helper'
 
 class ResultItemTest < ActiveSupport::TestCase
   ResultItem = BentoSearch::ResultItem
-  
+
   def test_has_custom_data
     r = ResultItem.new
-    
+
     assert_not_nil r.custom_data
     assert_kind_of Hash, r.custom_data
   end
-  
+
   def test_can_dup_and_set_attributes
     # Need to be able to dup and set at least basic attributes without
     # changing original. Used by SummonEngine for making sure
-    # openurl does not have highlighting tags in it when generated. 
-    
+    # openurl does not have highlighting tags in it when generated.
+
     r = ResultItem.new(:title => "original")
-    
+
     assert_equal "original", r.title
-    
+
     dup = r.dup
     dup.title = "new"
-    
+
     assert_equal "new", dup.title
     assert_equal "original", r.title
-    
-    assert_not_same dup, r        
+
+    assert_not_same dup, r
   end
-      
+
   def test_language
-    r = ResultItem.new(:title => "something", :language_code => "en")    
+    r = ResultItem.new(:title => "something", :language_code => "en")
     assert_equal "en",      r.language_code
     assert_equal "English", r.language_str
-    
+
     r = ResultItem.new(:title => "something", :language_code => "eng")
     assert_equal "eng",      r.language_code
     assert_equal "English", r.language_str
-    
+
     # language_str override
     r = ResultItem.new(:title => "something", :language_code => "en", :language_str => "Weird English")
     assert_equal "en",            r.language_code
     assert_equal "Weird English", r.language_str
-    
+
     # language_str only
     r = ResultItem.new(:title => "something", :language_str => "English")
     assert_nil r.language_code
     assert_equal "English", r.language_str
-    
+
   end
 
   def test_language_obj
@@ -64,15 +64,15 @@ class ResultItemTest < ActiveSupport::TestCase
 
     # neither is nil with no raise
     r = ResultItem.new(:title => "something")
-    assert_nil r.language_obj    
+    assert_nil r.language_obj
   end
 
-  
+
   def test_bad_language_code
     r = ResultItem.new(:title => "something", :language_code => "not_valid")
-    
+
     assert_equal "not_valid", r.language_code
     assert_nil r.language_str
   end
-  
+
 end
