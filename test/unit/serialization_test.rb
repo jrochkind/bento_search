6663f079acba9d13a8a942c539160e21fa4806d2
Jonathan Rochkind <jonathan@dnil.net>
2015-08-19 13:56:21 -0400

    put serialization tests together

--- test/unit/serialization_test.rb
+++ test/unit/serialization_test.rb
@@ -4,6 +4,7 @@ require 'test_helper'
 
 class SerializationTest < ActiveSupport::TestCase
   ResultItem = BentoSearch::ResultItem
+  MockEngine = BentoSearch::MockEngine
 
   setup do
     @init_hash = {:title => "something", 
@@ -35,9 +36,7 @@ class SerializationTest < ActiveSupport::TestCase
     @result_item = ResultItem.new(@init_hash)
   end
 
-  def test_serialization
-    
-
+  def test_item_serialization
     r = ResultItem.new(@init_hash)
 
     hash = r.internal_state_hash
@@ -62,7 +61,7 @@ class SerializationTest < ActiveSupport::TestCase
     end
   end
 
-  def test_json_serialization
+  def test_item_json_serialization
     json_str = @result_item.dump_to_json
 
     assert_kind_of String, json_str
@@ -74,7 +73,7 @@ class SerializationTest < ActiveSupport::TestCase
     end
   end
 
-  def test_html_safe_serialization
+  def test_item_html_safe_serialization
     r = ResultItem.new(:title => "<b>foo</b>".html_safe)
 
     r2 = ResultItem.load_json( r.dump_to_json )
@@ -119,7 +118,7 @@ class SerializationTest < ActiveSupport::TestCase
     assert_equal "http://example.org", l.url
   end
 
-  def test_year_and_date
+  def test_item_year_and_date
     r = ResultItem.new(:title => "foo", :year => 1991, :publication_date => Date.new(1991, 5, 1))
 
     r2 = ResultItem.load_json(  r.dump_to_json )
@@ -127,6 +126,83 @@ class SerializationTest < ActiveSupport::TestCase
     assert_equal 1991, r2.year
     assert_equal Date.new(1991, 5, 1), r2.publication_date
   end
+
+  class Results < ActionController::TestCase
+    test "serialize" do
+      engine = MockEngine.new(:id => "foo", 
+        :for_display => {:foo => "bar", :nested => {"one" => "two"}}
+      )
+      
+      results = engine.search("foo")
+
+      assert_kind_of Hash, results.internal_state_hash
+      assert_equal "foo", results.internal_state_hash["engine_id"]
+      assert_kind_of Array, results.internal_state_hash["result_items"]
+
+      assert_kind_of String, results.dump_to_json
+      assert_equal results.internal_state_hash, JSON.parse(results.dump_to_json)
+    end
+
+    test "de-serialize with no engine ID" do
+      engine = MockEngine.new(
+        :for_display => {:foo => "bar", :nested => {"one" => "two"}}
+      )      
+      results = engine.search("foo")
+
+      hash = results.internal_state_hash
+      restored = BentoSearch::Results.from_internal_state_hash(hash)
+      assert_kind_of BentoSearch::Results, restored
+      assert_equal results.size, restored.size
+      #assert_equal "foo", restored.engine_id
+
+      json_str = results.dump_to_json
+      assert_kind_of String, json_str
+      assert_kind_of BentoSearch::Results, BentoSearch::Results.load_json(json_str)
+    end
+
+    class RegisteredEngineTest < ActionController::TestCase
+      def setup
+        BentoSearch.register_engine("mock") do |config|
+          config.engine = "MockEngine"
+          config.for_display = {:foo => "bar", :nested => {"one" => "two"}, :decorator => "SomeDecorator"}
+        end
+      end      
+
+    
+      def teardown     
+        BentoSearch.reset_engine_registrations!
+      end
+
+      test "de-serializes with a registered engine ID, restoring context" do
+        mock_engine = BentoSearch.get_engine("mock")
+        results     = mock_engine.search("query")
+
+        json_str = results.dump_to_json
+        assert_kind_of String, json_str
+
+        restored = BentoSearch::Results.load_json(json_str)
+
+        assert_kind_of BentoSearch::Results, restored
+
+        assert_equal "mock", restored.engine_id
+        assert_equal mock_engine.configuration.for_display, restored.display_configuration
+
+        assert restored.length > 0
+
+        restored.each do |item|
+          assert_equal "mock", item.engine_id
+          assert_equal mock_engine.configuration.for_display, item.display_configuration
+          assert_equal "SomeDecorator", item.decorator
+        end
+
+      end
+
+    end
+
+  end
+
+
+
   
 
 end
