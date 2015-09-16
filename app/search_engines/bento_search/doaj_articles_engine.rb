a6d9f4fe38eecddc8338eaae6aa702691c2ab032
Jonathan Rochkind <jonathan@dnil.net>
2015-09-16 14:10:53 -0400

    basic DOAJ engine

---
+++ app/search_engines/bento_search/doaj_articles_engine.rb
@@ -0,0 +1,138 @@
+require 'httpclient'
+require 'http_client_patch/include_client'
+
+require 'json'
+
+module BentoSearch
+  class DoajArticlesEngine
+    include BentoSearch::SearchEngine
+    include ActionView::Helpers::SanitizeHelper
+
+
+    class_attribute :http_timeout
+    self.http_timeout = 10
+
+    extend HTTPClientPatch::IncludeClient
+    include_http_client do |client|
+      client.connect_timeout = client.send_timeout = client.receive_timeout = self.http_timeout
+    end
+
+    class_attribute :base_url
+    self.base_url = "https://doaj.org/api/v1/search/articles/"
+
+    def search_implementation(arguments)
+      query_url = args_to_search_url(arguments)
+
+      results = Results.new
+
+      response = http_client.get( query_url )
+
+      json = JSON.parse(response.body)
+
+      results.total_items = json["total"]
+
+      (json["results"] || []).each do |item_response|
+        results <<  hash_to_item(item_response)
+      end
+
+      return results
+    end
+
+
+    def args_to_search_url(arguments)
+      url = self.base_url + CGI.escape(escape_query(arguments[:query]))
+
+      query_args = {}
+      if arguments[:per_page]
+        query_args["pageSize"]  = arguments[:per_page]
+      end
+      if arguments[:page]
+        query_args["page"]      = arguments[:page]
+      end
+
+      query = query_args.to_query
+      url = url + "?" + query if query.present?
+
+      return url
+    end
+
+    # Converts from item found in DOAJ results to BentoSearch::ResultItem
+    def hash_to_item(hash)
+      item = ResultItem.new
+
+      bibjson = hash["bibjson"] || {}
+
+      item.unique_id  = hash["id"]
+
+      # Hard-code to Article, we don't get any format information
+      item.format     = "Article"
+
+      item.title      = bibjson["title"]
+
+
+      item.start_page = bibjson["start_page"]
+      item.end_page   = bibjson["end_page"]
+      
+      item.year       = bibjson["year"]
+      if (year = bibjson["year"].to_i) && (month = bibjson["month"].to_i)
+        if year != 0 && month != 0
+          item.publication_date = Date.new(bibjson["year"].to_i, bibjson["month"].to_i)
+        end
+      end      
+
+      item.abstract   = sanitize(bibjson["abstract"]) if bibjson.has_key?("abstract")
+
+      journal           = bibjson["journal"] || {}
+      item.volume       = journal["volume"]
+      item.issue        = journal["number"]
+      item.source_title = journal["title"]
+      item.publisher    = journal["publisher"]
+      item.language_str = journal["language"].try(:first)
+
+      (bibjson["identifier"] || []).each do |id_hash|
+        case id_hash["type"]
+        when "doi"
+          item.doi = id_hash["id"]
+        when "pissn"
+          item.issn = id_hash["id"]
+        end
+      end
+
+      (bibjson["author"] || []).each do |author_hash|
+        if author_hash.has_key?("name")
+          author = Author.new(:display => author_hash["name"])
+          item.authors << author
+        end
+      end
+
+      # I _think_ DOAJ articles results always only have one link,
+      # and it may always be of type 'fulltext'
+      link_hash             = (bibjson["link"] || []).first
+      if link_hash && link_hash["url"]
+        item.link             = link_hash["url"]
+        item.link_is_fulltext = true if link_hash["type"] == "fulltext"
+      end
+
+      return item
+    end
+
+    # Escape special chars in query, Doaj says it's elastic search,
+    # punctuation that needs to be escaped and how to escape (backslash)
+    # for ES documented here: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html
+    #
+    # This method does NOT return URI-escaped, it returns literal, escaped for ES. 
+    def escape_query(q)
+      q.gsub(/([\+\-\=\&\|\>\<\!\(\)\{\}\[\]\^\"\~\*\?\:\\\/])/) {|m| "\\#{$1}"}
+    end
+
+
+    ###########
+    # BentoBox::SearchEngine API
+    ###########
+
+    def max_per_page
+      100
+    end
+
+  end
+end
---
+++ test/search_engines/doaj_articles_engine_test.rb
@@ -0,0 +1,76 @@
+require 'test_helper'
+require 'uri'
+require 'cgi'
+
+class DoajArticlesEngineTest < ActiveSupport::TestCase
+  extend TestWithCassette
+
+  def setup    
+    @engine = BentoSearch::DoajArticlesEngine.new
+    # tell it not to send our bad API key
+  end
+
+  test_with_cassette("basic search", :doaj_articles) do
+    results = @engine.search("Breast cancer patients with lobular cancer more commonly have a father than a mother diagnosed with cancer")
+
+    assert_kind_of BentoSearch::Results, results    
+    assert ! results.failed?
+
+    assert_not_nil results.total_items
+    assert_equal 0, results.start
+    assert_equal 10, results.per_page
+    
+    assert_not_empty results
+    
+    first = results.first
+
+    assert_present first.unique_id
+    assert_equal "Article", first.format
+
+    assert_present first.title
+
+    assert_not_empty    first.authors
+    assert_not_empty    first.authors.first.display
+
+
+    assert_present first.source_title
+    assert_present first.issn
+    assert_present first.volume
+    assert_present first.issue
+
+    assert_present first.start_page
+
+    assert_present first.year
+    assert_present first.publication_date
+
+    assert_present first.abstract
+    assert first.abstract.html_safe?
+
+    assert_present first.link
+    assert first.link_is_fulltext?
+  end
+
+  test_with_cassette("pagination", :doaj_articles) do
+    results = @engine.search("cancer", :per_page => 20, :page => 3)
+    
+    assert ! results.failed?
+    
+    assert_equal 20, results.length
+    
+    assert_equal 20, results.size
+    assert_equal 40, results.start
+    assert_equal 20, results.per_page
+  end
+
+  test "escapes special chars" do
+    url = @engine.args_to_search_url(:query => "Me: And/Or You")
+
+    parsed = URI.parse(url)
+
+    last_path = parsed.path.split('/').last
+    last_path = CGI.unescape(last_path)
+
+    assert_equal "Me\\: And\\\/Or You", last_path
+  end
+
+end
