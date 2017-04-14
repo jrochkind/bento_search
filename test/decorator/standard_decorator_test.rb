d38788033568acfe6458f975ad631ef29d2c7669
Jonathan Rochkind <jonathan@dnil.net>
2017-04-14 15:56:09 -0400

    make minitest happier

--- test/decorator/standard_decorator_test.rb
+++ test/decorator/standard_decorator_test.rb
@@ -4,74 +4,74 @@ require 'test_helper'
 
 class StandardDecoratorTest < ActionView::TestCase
   include BentoSearch
-  
-  def decorator(hash = {})    
+
+  def decorator(hash = {})
     StandardDecorator.new(
       ResultItem.new(hash), view
     )
   end
-  
-  
+
+
   test "author with first and last" do
     author = Author.new(:last => "Smith", :first => "John")
-    
+
     str = decorator.author_display(author)
-    
-    assert_equal "Smith, J", str    
+
+    assert_equal "Smith, J", str
   end
-  
+
   test "author with display form and just last" do
     author = Author.new(:last => "Smith", :display => "Display Form")
-    
+
     str = decorator.author_display(author)
-    
+
     assert_equal "Display Form", str
   end
-  
+
   test "Author with just last" do
     author = Author.new(:last => "Johnson")
-    
+
     str = decorator.author_display(author)
-    
+
     assert_equal "Johnson", str
-    
+
   end
-  
+
   test "Missing title" do
     assert_equal I18n.translate("bento_search.missing_title"), decorator.complete_title
   end
-  
+
   test "language label nil if default" do
     I18n.with_locale(:'en-GB') do
-      item = decorator(:language_code => 'en')      
+      item = decorator(:language_code => 'en')
       assert_nil item.display_language
-      
+
       item = decorator(:language_code => 'es')
-      assert_equal "Spanish", item.display_language      
+      assert_equal "Spanish", item.display_language
     end
   end
 
   test "display_language works with just langauge_str" do
-     item = decorator(:language_str => 'German')      
+     item = decorator(:language_str => 'German')
      assert_equal "German", item.display_language
   end
-  
+
   test "display_format with nil format" do
     item = decorator(:format => nil, :format_str => nil)
-    
+
     display_format = item.display_format
-    
-    assert_nil display_format  
+
+    assert_nil display_format
   end
-  
+
   test "display_date" do
-    item = decorator(:year => 1900)    
+    item = decorator(:year => 1900)
     assert_equal "1900", item.display_date
-    
+
     d = Date.new(2010, 5, 5)
     item = decorator(:publication_date => d)
     assert_equal I18n.l(d, :format => "%d %b %Y"), item.display_date
-    
+
     # if volume and issue, only prints out year
     item = decorator(:publication_date => d, :volume => "101", :issue => "2")
     assert_equal I18n.l(d, :format => "%Y"), item.display_date
@@ -112,7 +112,7 @@ class StandardDecoratorTest < ActionView::TestCase
     assert_equal "snippet", item.render_summary, "use snippet if that's all that's there, even when configured for abstract"
 
     item = decorator()
-    assert_equal nil, item.render_summary, "Okay with no snippet or abstract"
+    assert_nil item.render_summary, "Okay with no snippet or abstract"
   end
-  
+
 end
