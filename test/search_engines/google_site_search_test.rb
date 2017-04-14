ab587c37021c5f95def81dd4e1edc772743a12c9
Jonathan Rochkind <jonathan@dnil.net>
2017-04-14 12:52:50 -0400

    whitespace normalize

--- test/search_engines/google_site_search_test.rb
+++ test/search_engines/google_site_search_test.rb
@@ -7,123 +7,123 @@ require 'cgi'
 # ENV GOOGLE_SITE_SEARCH_KEY and GOOGLE_SITE_SEARCH_CX
 class GoogleSiteSearchTest < ActiveSupport::TestCase
   extend TestWithCassette
-  
+
   @@api_key = ENV["GOOGLE_SITE_SEARCH_KEY"] || "DUMMY_API_KEY"
   @@cx      = ENV["GOOGLE_SITE_SEARCH_CX"] || "DUMMY_CX"
-  
+
   VCR.configure do |c|
     c.filter_sensitive_data("DUMMY_API_KEY", :google_site) { @@api_key }
     c.filter_sensitive_data("DUMMY_CX", :google_site) { @@cx }
   end
-  
+
   setup do
     @config = {:api_key => @@api_key, :cx => @@cx}
     @engine = BentoSearch::GoogleSiteSearchEngine.new(@config)
   end
-  
+
   test("basic query construction") do
     url = @engine.send(:construct_query, {:query => "hours policies"})
-    
-    query_params = CGI.parse( URI.parse(url).query )    
+
+    query_params = CGI.parse( URI.parse(url).query )
 
     assert_equal ["hours policies"], query_params["q"]
   end
-  
-  test("pagination construction") do    
+
+  test("pagination construction") do
     url = @engine.send(:construct_query, {:query => "books", :per_page => 5, :start => 10})
-    
+
     query_params = CGI.parse( URI.parse(url).query )
-    
+
     assert_equal ["5"], query_params["num"]
     assert_equal ["11"], query_params["start"]
   end
-  
+
   test("silently refuses to paginate too far") do
     # google won't let you paginate past ~10 pages, (101 - num). We silently
     # refuse
-    
+
     url = @engine.send(:construct_query, {:query => "books", :start => 110})
-    
+
     query_params = CGI.parse( URI.parse(url).query )
-    
+
     assert_equal ["91"], query_params["start"]
   end
-  
+
   test_with_cassette("pagination object is correct for actual page when you ask for too far", :google_site) do
     results = @engine.search("books", :start => 1000)
-    
+
     pagination = results.pagination
-    
+
     assert_equal 10, pagination.current_page
-    assert_equal 91, pagination.start_record    
+    assert_equal 91, pagination.start_record
   end
-  
+
   test("sort query construction") do
     url = @engine.send(:construct_query, {:query => "books", :sort => "date_desc"})
-    
+
     query_params = CGI.parse( URI.parse(url).query )
-    
+
     assert_equal ["date"], query_params["sort"]
   end
-  
-  test_with_cassette("basic smoke test", :google_site) do    
+
+  test_with_cassette("basic smoke test", :google_site) do
     results = @engine.search("books")
-    
+
     assert_present results
     assert_present results.total_items
     assert_kind_of Fixnum, results.total_items
-    
+
     first = results.first
-    
+
     assert_present first.title
     assert_present first.link
     assert_present first.abstract
-    assert_present first.journal_title # used as source_title for display url    
-    
+    assert_present first.journal_title # used as source_title for display url
+
     # no openurls for google, we decided
     assert_nil     BentoSearch::StandardDecorator.new(first, nil).to_openurl
   end
-  
+
   test_with_cassette("with highlighting", :google_site) do
     engine = BentoSearch::GoogleSiteSearchEngine.new(@config.merge(:highlighting => true))
-    
+
     results = engine.search("books")
-    
+
     first = results.first
 
-    assert first.title.html_safe?    
+    assert first.title.html_safe?
     assert first.abstract.html_safe?
-    assert first.journal_title.html_safe?    
+    assert first.journal_title.html_safe?
   end
-  
+
   test_with_cassette("without highlighting", :google_site) do
     engine = BentoSearch::GoogleSiteSearchEngine.new(@config.merge(:highlighting => false))
-    
+
     results = engine.search("books")
-    
+
     first = results.first
-    
-    assert ! first.title.html_safe?    
+
+    assert ! first.title.html_safe?
     assert ! first.abstract.html_safe?
-    assert ! first.journal_title.html_safe?        
+    assert ! first.journal_title.html_safe?
   end
-  
+
   test_with_cassette("empty result set", :google_site) do
     results = nil
     assert_nothing_raised { results = @engine.search('"adfa lakjdr xavier aldkfj 92323kjadf"') }
-    
+
     assert ! results.failed?
-    
-    assert results.empty?    
+
+    assert results.empty?
   end
-  
+
   test_with_cassette("gets format string", :google_site) do
     results = @engine.search("PDF")
 
     # assume at least one result had a PDF format, which it does
     # in our current VCR capture. For a new one, find a search where
-    # it does. 
-    assert_present(results.find_all {|i| i.format_str =~ /PDF/ }, "At least one result has PDF in format_str")    
+    # it does.
+    assert_present(results.find_all {|i| i.format_str =~ /PDF/ }, "At least one result has PDF in format_str")
   end
-  
+
 end
--- test/search_engines/scopus_engine_test.rb
+++ test/search_engines/scopus_engine_test.rb
@@ -4,32 +4,32 @@ require 'cgi'
 require 'uri'
 
 # Set shell env SCOPUS_KEY to your api key to test fresh http
-# connections, if you can't use the ones cached by VCR. 
+# connections, if you can't use the ones cached by VCR.
 
 class ScopusEngineTest < ActiveSupport::TestCase
   extend TestWithCassette
-  
+
   # Filter API key out of VCR cache for tag :scopus, which we'll use
-  # in this test. 
+  # in this test.
   @@api_key = (ENV["SCOPUS_KEY"] || "DUMMY_API_KEY")
   VCR.configure do |c|
     c.filter_sensitive_data("DUMMY_API_KEY", :scopus) { @@api_key }
   end
-  
+
   def setup
     @engine = BentoSearch::ScopusEngine.new(:api_key => @@api_key)
   end
-  
-  
+
+
   def test_construct_search_url
     url = @engine.send(:scopus_url, :query => "one two")
-    
+
     assert_equal "http://api.elsevier.com/content/search/index:SCOPUS?query=one+two&sort=refeid", url
   end
-  
+
   def test_construct_fielded_search_url
     url = @engine.send(:scopus_url, :query => "one two", :search_field => "AUTH")
-    
+
     assert_equal "http://api.elsevier.com/content/search/index:SCOPUS?query=AUTH%28one+two%29&sort=refeid", url
   end
 
@@ -46,66 +46,66 @@ class ScopusEngineTest < ActiveSupport::TestCase
     assert_includes query_components, "AUTH(John Smith)"
     assert_includes query_components, "TITLE(Terrible Diseases)"
   end
-  
+
   def test_construct_search_with_per_page
     url = @engine.send(:scopus_url, :query => "one two", :per_page => 30)
-    
+
     assert_equal "http://api.elsevier.com/content/search/index:SCOPUS?query=one+two&count=30&sort=refeid", url
   end
-  
+
   def test_construct_search_with_sort
     url = @engine.send(:scopus_url, :query => "one two", :sort => "date_desc")
-    
+
     assert_equal "http://api.elsevier.com/content/search/index:SCOPUS?query=one+two&sort=-datesort%2C%2Bauth", url
-    
+
     url = @engine.send(:scopus_url, :query => "one two", :sort => "relevance")
-    
+
     assert_equal "http://api.elsevier.com/content/search/index:SCOPUS?query=one+two&sort=refeid", url
-    
+
   end
-  
+
   def test_construct_default_relevance_sort
     url_implicit = @engine.send(:scopus_url, :query => "one two")
     url_explicit = @engine.send(:scopus_url, :query => "one two", :sort => "relevance")
-    
+
     assert_equal url_explicit, url_implicit
   end
-    
-  
+
+
   def test_construct_with_pagination
     url = @engine.send(:scopus_url, :query => "one two", :start => 20, :per_page => 10)
-    
+
     query_hash = CGI.parse(URI.parse(url).query)
-    
+
     assert_equal ["20"], query_hash["start"]
-    assert_equal ["10"], query_hash["count"]    
+    assert_equal ["10"], query_hash["count"]
   end
-  
-  
+
+
   test_with_cassette("bad api key should return error response", :scopus) do
     @engine = BentoSearch::ScopusEngine.new(:api_key => "BAD_KEY_ERROR")
-    
+
     results = @engine.search(:query => "cancer")
 
-    assert results.failed?, "response.failed? should be"  
+    assert results.failed?, "response.failed? should be"
 
     assert_present results.error[:error_info]
     assert_includes results.error[:error_info], "AUTHORIZATION_ERROR"
   end
-  
+
   test_with_cassette("simple search", :scopus) do
     results = @engine.search(:query => "cancer")
-    
+
     assert_not_nil results.total_items, "total_items not nil"
     assert_kind_of Fixnum, results.total_items
-    
+
     assert_not_nil results.start, "start not nil"
     assert_not_nil results.per_page, "per_page not nil"
-    
+
     assert_equal 10, results.length
-    
+
     sample_result = results.first
-    
+
     assert_present sample_result.title
     assert_present sample_result.link
     assert_present sample_result.journal_title
@@ -114,37 +114,37 @@ class ScopusEngineTest < ActiveSupport::TestCase
     assert_present sample_result.volume
     assert_present sample_result.issue
     assert_present sample_result.start_page
-    
-    assert_present sample_result.authors    
-    
+
+    assert_present sample_result.authors
+
     assert_present sample_result.format
-    
+
     assert_present sample_result.unique_id
-    
+
   end
-  
+
   test_with_cassette("zero results search", :scopus) do
     results = @engine.search(:query => "aldfjkadf lakdj zdfzzzz")
     assert ! results.failed?, "results not marked failed"
     assert_equal 0, results.size
   end
-  
+
   test_with_cassette("escaped chars", :scopus) do
     results = @engine.search(:query => "monkey:(brain)")
-    
+
     assert ! results.failed?, "results not marked failed"
   end
-    
-    
-  
+
+
+
   test_with_cassette("fielded search", :scopus) do
     results = @engine.search(:query => "cancer", :semantic_search_field => :title)
 
-    assert results.first.title.downcase.include?("cancer"), "Title includes query term"    
+    assert results.first.title.downcase.include?("cancer"), "Title includes query term"
   end
-  
+
   test_with_cassette("multi-field search", :scopus) do
-    results = @engine.search(:query => 
+    results = @engine.search(:query =>
       { :title => "Protein measurement with the folin phenol reagent",
         :author => "Lowry",
         :issn => "0021-9258"
@@ -168,12 +168,12 @@ class ScopusEngineTest < ActiveSupport::TestCase
     })
 
     assert ! results.failed?
-    assert_equal 1, results.total_items 
+    assert_equal 1, results.total_items
 
     item = results.first
 
     assert item.title.start_with?("Architects and planners in the middle of a road war")
   end
-    
-  
+
+
 end
